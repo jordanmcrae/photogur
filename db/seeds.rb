f7e810b2d619fc3fb80fd365a37700693a1be8a9
Jordan <jordanmcrae@hotmail.com>
2015-04-15 16:41:25 -0400

    Added Image model in database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Image.create!(
+  :artist => "Jordan McRae",
+  :title => "Pyro",
+  :url => "https://farm8.staticflickr.com/7459/16329662786_21e4c4ae88_b.jpg"
+  )
+
+Image.create!(
+  :artist => "Jordan McRae",
+  :title => "Sarasota Sunset",
+  :url => "https://farm9.staticflickr.com/8602/16233401097_a4baab514e_b.jpg"
+  )
+
+Image.create!(
+  :artist => "Jordan McRae",
+  :title => "Banff",
+  :url => "https://farm6.staticflickr.com/5616/15176835664_785e2fcbb2_b.jpg"
+  )
+
+Image.create!(
+  :artist => "Jordan McRae",
+  :title => "Montreal",
+  :url => "https://farm4.staticflickr.com/3956/15654805062_b0fdb299d7_b.jpg"
+  )
